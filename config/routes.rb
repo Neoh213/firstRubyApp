fda2f48c22745125cbe817e9648c5d859af121d9
Neoh213 <djrumph@gmail.com>
2018-02-09 23:17:25 -0500

    nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 #route route
-root 'posts#index'
+root 'posts#index', as: 'home'
 
 #regular route
-get 'about' => 'page#about'
+get 'about' => 'page#about', as: 'about'
 
 resources :posts
 
