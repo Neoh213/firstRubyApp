1294f521eadc701529553496e12ee77c4de5786d
Neoh213 <djrumph@gmail.com>
2018-02-10 22:25:00 -0500

    comments added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
 
   def create
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.create(comment_params)
     redirect_to post_path(@post)
   end
 
-  private comment_params
-  params.require(:comment).permind(:username,:body)
+  private def comment_params
+  params.require(:comment).permit(:username,:body)
   end
 end
